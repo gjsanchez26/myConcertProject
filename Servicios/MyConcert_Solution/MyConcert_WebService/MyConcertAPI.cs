915e646363f4c5c06b8bcc20c63329d99866c853
Ernesto Ulate <netou18@gmail.com>
2017-06-06 16:08:58 -0600

    Servidor web - primer - master

---
+++ Servicios/MyConcert_Solution/MyConcert_WebService/MyConcertAPI.cs
@@ -0,0 +1,12 @@
+﻿using MyConcert_WebService.server;
+
+namespace MyConcert_WebService
+{
+    class MyConcertAPI
+    {
+        static void Main(string[] args)
+        {
+            Server nuevoServidorWeb = new Server();
+        }
+    }
+}
