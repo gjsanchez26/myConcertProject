a427a2e09c7268cca15fef279b36cd0d55e25c7a
jfsancho <javiersanchomarin@hotmail.com>
2017-06-07 00:01:34 -0600

    manejadorBD inicial

---
+++ Servicios/MyConcert_Solution/MyConcert_WebService/ManejadorBD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConcert_WebService
+{
+    public class ManejadorBD
+    {
+        public usuarios obtenerUsuario(string username)
+        {
+            usuarios us = null;
+            using (myconcertEntities context = new myconcertEntities())
+            {
+                us = context.usuarios.FirstOrDefault(r=> r.username==username);
+            }
+            return us;
+        }
+    }
+}
