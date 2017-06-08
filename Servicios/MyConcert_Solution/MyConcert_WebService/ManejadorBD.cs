0f9522edc37d21f278925f910f85deb244120763
jfsancho <javiersanchomarin@hotmail.com>
2017-06-07 22:59:30 -0600

    Nuevos metodos CRUD

--- Servicios/MyConcert_Solution/MyConcert_WebService/ManejadorBD.cs
+++ Servicios/MyConcert_Solution/MyConcert_WebService/ManejadorBD.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,5 +46,69 @@ namespace MyConcert_WebService
                 Console.Write(ex.InnerException.ToString());
             }
         }
+        public List<bandas> obtenerBandasNoCartelera(eventos cartelera)
+        {
+            List<bandas> bandasCarte=null;
+            string query = @"SELECT B.PK_bandas, B.nombreBan, B.FK_BANDAS_ESTADOS " +
+                            "FROM bandas as B INNER JOIN categoriasevento as CE " +
+                            "on CE.FK_CATEGORIASEVENTO_BANDAS = B.PK_bandas " +
+                            "where CE.FK_CATEGORIASEVENTO_EVENTOS != ?PK_eventos";
+
+            MySqlParameter[] parameters = {
+            new MySqlParameter("?PK_eventos", cartelera.PK_eventos)
+        };
+            try
+            {
+                using (myconcertEntities context = new myconcertEntities())
+                {
+                  
+                    bandasCarte = context.bandas.SqlQuery(query, parameters).ToList<bandas>();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.InnerException.ToString());
+            }
+            return bandasCarte;
+        }
+        
+        public int getCantidadComentarios (bandas banda)
+        {
+            int cantidadComen = 0;
+            try
+            {
+                using (myconcertEntities context = new myconcertEntities())
+                {
+
+                    cantidadComen = context.comentarios.Count(r => r.FK_COMENTARIOS_BANDAS == banda.PK_bandas);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.InnerException.ToString());
+            }
+            return cantidadComen;
+        }
+        
+        public float getCalificacion(bandas banda)
+        {
+            float calificacion = 0;
+            try
+            {
+                using (myconcertEntities context = new myconcertEntities())
+                {
+
+                    calificacion = (float)context.comentarios.Where(r=> r.FK_COMENTARIOS_BANDAS==banda.PK_bandas).Average(r=>r.calificacion);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.InnerException.ToString());
+            }
+            return calificacion;
+        } 
     }
 }
