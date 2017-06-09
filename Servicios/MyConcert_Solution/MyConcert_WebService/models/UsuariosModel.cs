ec3348450c855ac88e9dda4781cabaab62e918bb
Ernesto Ulate <netou18@gmail.com>
2017-06-09 00:54:40 -0600

    Manejo de errores.

--- Servicios/MyConcert_Solution/MyConcert_WebService/models/UsuariosModel.cs
+++ Servicios/MyConcert_Solution/MyConcert_WebService/models/UsuariosModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyConcert_WebService.res.resultados;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +11,57 @@ namespace MyConcert_WebService.models
     public class UsuariosModel
     {
         private ManejadorBD _manejador;
+        private FabricaRespuestas creador = new FabricaRespuestas();
+        private Respuesta respuesta;
 
         public UsuariosModel()
         {
             _manejador = new ManejadorBD();
         }
 
-        public usuarios getUsuarioPorNombreDeUsuario(string pUsuario)
+        public Respuesta comprobarInicioSesion(string pUsername, string pPassword)
         {
-            usuarios user = _manejador.obtenerUsuario(pUsuario);
-            return user;
+            usuarios usuarioActual;
+            try
+            {
+                usuarioActual = getUsuarioPorNombreDeUsuario(pUsername);
+            } catch(Exception e)
+            {
+                return respuesta = creador.crearRespuesta(false, "Error en conexión con el servidor. Intente de nuevo.");
+            }
+            
+
+            if (usuarioActual == null)                                  //Si no existe el nombre de usuario introducido.
+            {
+                respuesta = creador.crearRespuesta(false, "Usuario no existente.");
+            }
+            else
+            {
+                if (usuarioActual.contraseña != pPassword)              //Si la contrasena es incorrecta.
+                {
+                    respuesta = creador.crearRespuesta(false, "Contraseña incorrecta. Intente de nuevo.");
+                }
+                else                                                  //Si el usuario y contrasena son validos.
+                {
+                    respuesta = creador.crearRespuesta(false, JObject.FromObject(usuarioActual));
+                }
+            }
+
+            return respuesta;
+        }
+
+        private usuarios getUsuarioPorNombreDeUsuario(string pUsuario)
+        {
+            usuarios user = null;
+            try
+            {
+                user = _manejador.obtenerUsuario(pUsuario);
+                return user;
+            } catch(Exception e)
+            {
+                throw(e);
+            }
+            
         }
     }
 }
