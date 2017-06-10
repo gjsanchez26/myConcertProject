c9d892aac7517ec3dbecb0c2553dd9f94173c764
Ernesto Ulate <netou18@gmail.com>
2017-06-09 23:55:04 -0600

    Reset

--- Servicios/MyConcert_Solution/MyConcert_WebService/models/UtilidadesModel.cs
+++ Servicios/MyConcert_Solution/MyConcert_WebService/models/UtilidadesModel.cs
@@ -11,30 +11,30 @@ namespace MyConcert_WebService.models
     {
         ManejadorBD _manejador = new ManejadorBD();
 
-        public Respuesta getUniversidades()
-        {
-            List<universidades> listaUniversidades = _manejador.obtenerUniversidades();
+        //public Respuesta getUniversidades()
+        //{
+        //    List<universidades> listaUniversidades = _manejador.obtenerUniversidades();
 
 
 
-            return new Respuesta();
-        }
+        //    return new Respuesta();
+        //}
 
-        public Respuesta getPaises()
-        {
-            List<paises> listaPaises = _manejador.obtenerPaises();
-
-
-
-            return new Respuesta();
-        }
-
-        public Respuesta getGenerosMusicales()
-        {
-            List<generos> listaGenerosMusicales = _manejador.obtenerGeneros();
+        //public Respuesta getPaises()
+        //{
+        //    List<paises> listaPaises = _manejador.obtenerPaises();
 
 
-            return new Respuesta();
-        }
+
+        //    return new Respuesta();
+        //}
+
+        //public Respuesta getGenerosMusicales()
+        //{
+        //    List<generos> listaGenerosMusicales = _manejador.obtenerGeneros();
+
+
+        //    return new Respuesta();
+        //}
     }
 }
