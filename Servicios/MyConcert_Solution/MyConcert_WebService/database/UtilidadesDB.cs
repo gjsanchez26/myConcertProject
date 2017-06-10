363cbf699bde44cff3ff77503aed94ca784e56aa
jfsancho <javiersanchomarin@hotmail.com>
2017-06-09 20:58:53 -0600

    obtener UNIVERSIDADES, PAISES, GENEROS

--- Servicios/MyConcert_Solution/MyConcert_WebService/database/UtilidadesDB.cs
+++ Servicios/MyConcert_Solution/MyConcert_WebService/database/UtilidadesDB.cs
@@ -21,9 +21,26 @@ namespace MyConcert_WebService.database
 
         }
         //OBTENER LISTA DE OBJETOS
-        public List<generos> obtenerGeneros()
+
+        private Pais convertirpaisesAPais(paises pPais)
+        {
+            int id = pPais.PK_paises;
+            string nombre = obtenerPais(id).pais;
+            Pais pais = new Pais(id,nombre);
+            return pais;
+        }
+        
+        private GeneroMusical convertirgenerosAGenero(generos pGenero)
+        {
+            int id = pGenero.PK_generos;
+            string nombre = obtenerGenero(id).genero;
+            GeneroMusical gene = new GeneroMusical(id, nombre);
+            return gene;
+        }
+        public GeneroMusical[] obtenerGeneros()
         {
             List<generos> gen = null;
+            GeneroMusical[] arGenMus=null;
 
 
             try
@@ -34,18 +51,27 @@ namespace MyConcert_WebService.database
                     gen = context.generos.ToList();
 
                 }
+                arGenMus = new GeneroMusical[gen.Count];
+                int c = 0;
+                foreach (generos i in gen)
+                {
+                    arGenMus[c] = convertirgenerosAGenero(i);
+                    c++;
+                }
+
+
             }
             catch (Exception ex)
             {
                 Console.Write(ex.InnerException.ToString());
             }
-            return gen;
+            return arGenMus;
         }
 
-        public List<paises> obtenerPaises()
+        public Pais[] obtenerPaises()
         {
             List<paises> lista = null;
-
+            Pais[] arreglo = null;
 
             try
             {
@@ -55,18 +81,25 @@ namespace MyConcert_WebService.database
                     lista = context.paises.ToList();
 
                 }
+                arreglo = new Pais[lista.Count];
+                int c = 0;
+                foreach (paises i in lista)
+                {
+                    arreglo[c] = convertirpaisesAPais(i);
+                    c++;
+                }
             }
             catch (Exception ex)
             {
                 Console.Write(ex.InnerException.ToString());
             }
-            return lista;
+            return arreglo;
         }
 
-        public List<universidades> obtenerUniversidades()
+        public Universidad[] obtenerUniversidades()
         {
             List<universidades> lista = null;
-
+            Universidad[] arreglo = null;
 
             try
             {
@@ -75,6 +108,13 @@ namespace MyConcert_WebService.database
 
                     lista = context.universidades.ToList();
 
+                }
+                arreglo = new Universidad[lista.Count];
+                int c = 0;
+                foreach (universidades i in lista)
+                {
+                    arreglo[c] = convertiruniversidadesAUniversidad(i);
+                    c++;
                 }
             }
             catch (Exception ex)
@@ -124,6 +164,7 @@ namespace MyConcert_WebService.database
             return obj;
         }
 
+        
 
         public paises obtenerPais(string pais)
         {
@@ -208,6 +249,24 @@ namespace MyConcert_WebService.database
                 using (myconcertEntities context = new myconcertEntities())
                 {
                     ge = context.generos.FirstOrDefault(g => g.PK_generos == PK_genero);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.InnerException.ToString());
+            }
+            return ge;
+        }
+        public generos obtenerGenero(string genero)
+        {
+            generos ge = null;
+            try
+            {
+
+                using (myconcertEntities context = new myconcertEntities())
+                {
+                    ge = context.generos.FirstOrDefault(g => g.genero == genero);
                 }
 
             }
